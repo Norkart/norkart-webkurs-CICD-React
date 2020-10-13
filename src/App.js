fabea7506b9faa3552fe4e699346610ef18374a4
Kari.Meling.Johannessen <karjoh@norkart.no>
2020-10-13 08:57:29 +0200

    add map component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
-import "./App.css";
+import Header from "./components/Header";
+import MapboxGLMap from "./components/MapboxGLMap";
+
+const styles = {
+  overflow:'hidden'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">KART!!</header>
+    <div style={styles}>
+      <Header/>
+      <MapboxGLMap />
     </div>
   );
 }
---
+++ src/components/MapboxGLMap.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const styles = {
+  width: "100%",
+  height: "calc(100vh - 80px)",
+  position: "absolute"
+};
+
+const MapboxGLMap = () => {
+  const [map, setMap] = useState(null);
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
+    const initializeMap = ({ setMap, mapContainer }) => {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+        center: [10.408773,63.422091],
+        zoom: 10
+      });
+
+      map.on("load", () => {
+        setMap(map);
+        map.resize();
+      });
+    };
+
+    if (!map) initializeMap({ setMap, mapContainer });
+  }, [map]);
+
+  return <div ref={el => (mapContainer.current = el)} style={styles} />;
+};
+
+export default MapboxGLMap;
