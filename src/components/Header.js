a106bc6ba6985aeba0339081dbd95c52c587e84e
Kari Meling Johannessen <karjoh@norkart.no>
2022-10-05 19:24:18 +0200

    try

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const styles = {
 const Header = () => {
     return (
         <div style={styles}>
-            <header>KART baby!</header>
+            <header>KART????</header>
         </div>
     )
 }
