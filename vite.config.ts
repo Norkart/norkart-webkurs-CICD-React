581bd1f5a6b4271f16a9cdc80122a7966b75e01b
Sindre Solheim <sinsol@norkart.no>
2025-09-25 11:32:28 +0200

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-bedpress/',
+  base: '/norkart-workshop-React-CICD/',
   plugins: [react()],
 });
