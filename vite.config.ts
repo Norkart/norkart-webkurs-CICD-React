63e0a1189a6a53186e4b700d108193648aeeebe1
Sindre Solheim <sinsol@norkart.no>
2025-09-25 09:12:22 +0200

    use dist folder instead of build folder..

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/norkart-workshop-React-CICD/',
   server: {
     port: 3000,
     host: 'localhost'
