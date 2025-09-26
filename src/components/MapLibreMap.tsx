f625f60cbce0b0f850228b42c5e5920f5178f8ee
Jarl Sterkenburg <jarste@norkart.no>
2025-09-26 10:45:45 +0200

    Add API code and example

--- src/components/MapLibreMap.tsx
+++ src/components/MapLibreMap.tsx
@@ -1,34 +1,33 @@
-import type { MapLayerMouseEvent } from 'maplibre-gl';
-import 'maplibre-gl/dist/maplibre-gl.css';
-import { RMap } from 'maplibre-react-components';
-import { getHoydeFromPunkt } from '../api/getHoydeFromPunkt';
-import { useEffect, useState } from 'react';
-
+import type { MapLayerMouseEvent } from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+import { RMap } from "maplibre-react-components";
+import { getHoydeFromPunkt } from "../api/getHoydeFromPunkt";
+import { useEffect, useState } from "react";
 
 const trondheim: [number, number] = [10.40565401, 63.4156575];
 
 export const MapLibreMap = () => {
-    const [hoyde, setHoydeAtPunkt] = useState<undefined | number>(undefined);
+  const [hoyde, setHoydeAtPunkt] = useState<undefined | number>(undefined);
 
-    useEffect(() => {
-        console.log(hoyde);
-    }, [hoyde]);
+  useEffect(() => {
+    console.log(hoyde);
+  }, [hoyde]);
 
-    const onMapClick = async (e: MapLayerMouseEvent) => {
-        const hoyder = await getHoydeFromPunkt(e.lngLat.lng, e.lngLat.lat);
-        setHoydeAtPunkt(hoyder[0].Z)
-    }
+  const onMapClick = async (e: MapLayerMouseEvent) => {
+    const hoyder = await getHoydeFromPunkt(e.lngLat.lng, e.lngLat.lat);
+    setHoydeAtPunkt(hoyder[0].Z);
+  };
 
-    return (
-        <div style={{ position: 'relative', height: '80vh', width: '100%' }}>
-            <RMap
-                minZoom={6}
-                initialCenter={trondheim}
-                initialZoom={12}
-                mapStyle="https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json"
-                style={{ height: '100%', width: '100%' }}
-                onClick={onMapClick}
-            />
-        </div>
-    );
-}
+  return (
+    <div style={{ position: "relative", height: "1000px", width: "100%" }}>
+      <RMap
+        minZoom={6}
+        initialCenter={trondheim}
+        initialZoom={12}
+        mapStyle="https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json"
+        style={{ height: "100%", width: "100%" }}
+        onClick={onMapClick}
+      />
+    </div>
+  );
+};
