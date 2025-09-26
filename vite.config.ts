7e50ad67b13baf98bc836082b54005e67a23257b
Sindre Solheim <sinsol@norkart.no>
2025-09-26 09:25:53 +0200

    try to use token the new way

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/norkart-webkurs-CICD-React/',
+  base: '/norkart-workshop-React-CICD/',
   plugins: [react()],
 });
