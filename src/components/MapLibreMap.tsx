c5a9be9d342f9b2d3193c934689846603c990876
Oskar Holm <oskhol@norkart.no>
2025-09-29 10:03:18 +0200

    Cleanup

--- src/components/MapLibreMap.tsx
+++ src/components/MapLibreMap.tsx
@@ -3,35 +3,10 @@ import "maplibre-gl/dist/maplibre-gl.css";
 import { RMap } from "maplibre-react-components";
 import "./MapLibreMap.css";
 import { getHoydeFromPunkt } from "../api/getHoydeFromPunkt";
-import {
-  useEffect,
-  useState,
-  type CSSProperties,
-  type PropsWithChildren,
-} from "react";
+import { useEffect, useState } from "react";
+import { Overlay } from "./Overlay";
 
 const TRONDHEIM_COORDS: [number, number] = [10.40565401, 63.4156575];
-
-function Overlay({
-  children,
-  style,
-}: PropsWithChildren & { style?: CSSProperties }) {
-  return (
-    <div
-      style={{
-        position: "relative",
-        backgroundColor: "white",
-        borderRadius: "20px",
-        padding: "20px",
-        width: "fit-content",
-        boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
-        ...style,
-      }}
-    >
-      {children}
-    </div>
-  );
-}
 
 export const MapLibreMap = () => {
   const [hoyde, setHoydeAtPunkt] = useState<undefined | number>(undefined);
@@ -57,14 +32,6 @@ export const MapLibreMap = () => {
       onClick={onMapClick}
     >
       <Overlay>
-        <h2>Dette er et overlay</h2>
-        <p>Legg til funksjonalitet knyttet til kartet.</p>
-      </Overlay>
-      <Overlay>
-        <h2>Dette er et overlay</h2>
-        <p>Legg til funksjonalitet knyttet til kartet.</p>
-      </Overlay>
-      <Overlay style={{ position: "absolute", bottom: 0 }}>
         <h2>Dette er et overlay</h2>
         <p>Legg til funksjonalitet knyttet til kartet.</p>
       </Overlay>
