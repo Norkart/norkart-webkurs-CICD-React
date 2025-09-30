68d4bfebbb226aa7cb7206cb0c4ca897c245fca4
Jarl Sterkenburg <jarste@norkart.no>
2025-09-30 10:52:39 +0200

    Fix typos

--- src/api/getBygningAtPunkt.ts
+++ src/api/getBygningAtPunkt.ts
@@ -1,12 +1,10 @@
 export const getBygningAtPunkt = async (x: number, y: number) => {
     const apiKey = import.meta.env.VITE_API_KEY;
-    const query = `https://bygning.api.norkart.no/bygninger/byposition?x=${x}&y=${y}&MaxRadius=1`;
-
-    return undefined;
+    const query = `https://bygning.api.norkart.no/bygninger/byposition?x=${x}&y=${y}&MaxRadius=1&GeometryTextFormat=GeoJson&IncludeFkbData=true`;
 
     // TODO: Fullfør/endre koden for hente og returnere bygningsdata på et punkt
 
-    // Hint: Du kan se på getAdressweDeomSearchText og getHoydeFromPunkt for å få en idé om hvordan
+    // Hint: Du kan se på getAdresseFromSearchText og getHoydeFromPunkt for å få en idé om hvordan
     // dette kan gjøres.
 
     // Merk at dette er en GET request, og ikke en POST request!
